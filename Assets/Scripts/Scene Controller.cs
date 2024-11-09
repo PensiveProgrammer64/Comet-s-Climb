5e64d85c39347a9e1d3952f7010fc3aaf90ac67f
MattLuL2 <90647361+MattLuL2@users.noreply.github.com>
2024-11-09 14:53:13 -0500

    Added Endscreen activation on Collision

---
+++ Assets/Scripts/Scene Controller.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void PlayGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void PlayGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
